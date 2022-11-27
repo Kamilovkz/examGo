34448b85e7e7d4e6d9d75b3b25a76f60dc649b22
nkamilov <001kamilov@gmail.com>
2022-11-28 00:12:30 +0600

    Tab mult finish

--- tabmult/main.go
+++ tabmult/main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/01-edu/z01"
 )
 
 func main() {
-	number := os.Args[1:]
-	if len(number) != 1 {
+	numb := os.Args[1:]
+	if len(numb) != 1 {
 		z01.PrintRune('\n')
 		return
 	}
-	for i := '0'; i <= '9'; i++ {
+	for i := '1'; i <= '9'; i++ {
 		z01.PrintRune(i)
+		z01.PrintRune(' ')
+		z01.PrintRune('x')
+		z01.PrintRune(' ')
+		for _, i := range numb[0] {
+			z01.PrintRune(i)
+		}
+		z01.PrintRune(' ')
+		z01.PrintRune('=')
+		z01.PrintRune(' ')
+		printAtoi(numb[0], i)
 		z01.PrintRune('\n')
+	}
+}
+
+func printAtoi(s string, r rune) {
+	number, _ := strconv.Atoi(s)
+	res := number * int(r-48)
+	var result []rune
+	for res != 0 {
+		result = append(result, rune(res%10+48))
+		res /= 10
+	}
+	for i := len(result) - 1; i >= 0; i-- {
+		z01.PrintRune(result[i])
 	}
 }
