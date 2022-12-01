0dc20b7d04e2172a0f17a294ca1acd7a5c47b86a
nkamilov <001kamilov@gmail.com>
2022-12-01 11:35:36 +0600

    Add repeatAlpha

--- repeatalpha/main.go
+++ repeatalpha/main.go
@@ -9,9 +9,30 @@ import (
 func main() {
 	if len(os.Args) != 2 {
 		return
-	} else if os.Args[1] == "" {
-		z01.PrintRune('\n')
-		return
 	}
-	// Not finished
+	s := os.Args[1]
+	alpha1 := " abcdefghijklmnopqrstuvwxyz"
+	alpha2 := " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	var count int
+	for _, r := range s {
+		for i := range alpha1 {
+			if r == rune(alpha1[i]) {
+				count = i
+			}
+		}
+		for i := range alpha2 {
+			if r == rune(alpha2[i]) {
+				count = i
+			}
+		}
+		if count != 0 {
+			for i := 0; i < count; i++ {
+				z01.PrintRune(r)
+			}
+		} else {
+			z01.PrintRune(r)
+		}
+	}
+	z01.PrintRune(10)
 }
