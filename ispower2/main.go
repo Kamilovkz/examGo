485e82a1f3abfaa22607f87f0449e090e565b2e1
nkamilov <001kamilov@gmail.com>
2022-12-02 12:46:13 +0600

    Udpate isPower2

--- ispower2/main.go
+++ ispower2/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
 	if len(os.Args) != 2 {
 		return
 	}
-	arg := os.Args[1]
-	fmt.Println(arg)
+	arg := os.Args[1:]
+	n := Atoi(arg[0])
+	if n&(n-1) == 0 && n > 0 {
+		result := "true"
+		for _, w := range result {
+			z01.PrintRune(w)
+		}
+		z01.PrintRune(10)
+	} else {
+		result := "false"
+		for _, w := range result {
+			z01.PrintRune(w)
+		}
+		z01.PrintRune(10)
+	}
+}
+
+func Atoi(args string) int {
+	s := 0
+	temp := 1
+	if args[0] == '-' {
+		temp = -1
+		args = args[1:]
+	}
+	for v := range args {
+		s = s * 10
+		s = s + int(args[v]-48)
+	}
+	return s * temp
 }
