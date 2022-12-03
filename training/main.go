6e954a6351534c6e29b5b8bf0c9c1b3c34dbf8f5
nkamilov <001kamilov@gmail.com>
2022-12-03 21:10:51 +0600

    training

--- training/main.go
+++ training/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	Chunk([]int{}, 10)
 	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
@@ -9,5 +13,18 @@ func main() {
 }
 
 func Chunk(slice []int, size int) {
-
+	if len(slice) == 0 {
+		fmt.Println(slice)
+		return
+	} else if size == 0 {
+		fmt.Println()
+		return
+	}
+	var res [][]int
+	for len(slice) > size {
+		res = append(res, slice[:size])
+		slice = slice[size:]
+	}
+	res = append(res, slice)
+	fmt.Println(res)
 }
