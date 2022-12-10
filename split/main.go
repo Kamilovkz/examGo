cfe8ab24f9bdf2257c6c8d1037e669904d80d134
nkamilov <001kamilov@gmail.com>
2022-12-11 00:51:57 +0600

    Update split

--- split/main.go
+++ split/main.go
@@ -10,5 +10,23 @@ func main() {
 }
 
 func Split(s, sep string) []string {
-
+	l := len(sep)
+	var result []string
+	for i := 0; i < len(s)-l-1; i++ {
+		if s[i:i+l] == sep && i < len(s)-1 {
+			result = append(result, s[:i])
+			s = s[i+l:]
+			i = 0
+		}
+	}
+	result = append(result, s)
+	var result2 []string
+	for _, word := range result {
+		if word != "" && word != " " {
+			result2 = append(result2, word)
+		} else {
+			continue
+		}
+	}
+	return result2
 }
