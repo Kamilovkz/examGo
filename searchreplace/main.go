2ea855bf3c9ca5cee80ca90995b871a03031d5d9
Kamilovkz <001kamilov@gmail.com>
2023-01-24 17:35:15 +0600

    update scripts

--- searchreplace/main.go
+++ searchreplace/main.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	if len(os.Args) == 4 {
-		for _, d := range os.Args[1] {
-			if d == []rune(os.Args[2])[0] {
-				d = []rune(os.Args[3])[0]
+		for _, word := range os.Args[1] {
+			if word == []rune(os.Args[2])[0] {
+				word = []rune(os.Args[3])[0]
 			}
-			z01.PrintRune(d)
+			z01.PrintRune(word)
 		}
 		z01.PrintRune('\n')
 	}
